320083ce7425d536b6e5c33482c23e7297e227cf
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-04 23:09:12 +0530

    Removed xz compression from release.sh

--- release.sh
+++ release.sh
@@ -8,14 +8,11 @@ echo "Compressing the Debian VM disk image..."
 
 ship --vm compress debian-vm-base 
 
-echo "Starting the xz compression of the Debian disk image to generate the release package for 'debian-vm-base'..."
+echo "Copying the Debian VM disk image to generate the release package for 'debian-vm-base'..."
 
 DISK_IMAGE=$(sudo virsh domblklist debian-vm-base | grep .qcow2 | awk '{print $2}')
 
-xz -9 -z "$DISK_IMAGE"
-
-echo "Moving the compressed disk image to the output directory..."
-
-mv "$DISK_IMAGE.xz" output/debian.qcow2.xz
+cp "$DISK_IMAGE" output/debian.qcow2
 
 echo "The release package for 'debian-vm-base' has been generated successfully!"
+
