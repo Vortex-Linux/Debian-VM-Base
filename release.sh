0e1758aa034cc15a42136c8eb0e8f978e13cee68
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-04 22:16:49 +0530

    Made release.sh upto date and modified for debian

--- release.sh
+++ release.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo "Starting the compression of the Arch disk image to generate the release package for 'arch-vm-base'..."
+echo "Shutting down the Debian VM..." 
 
-DISK_IMAGE=$(sudo virsh domblklist arch-vm-base | grep .qcow2 | awk '{print $2}')
+echo y | ship --vm shutdown debian-vm-base 
 
-xz -z "$DISK_IMAGE"
+echo "Compressing the Debian VM disk image..."
 
-mv "$DISK_IMAGE.xz" output/archlinux.qcow2.xz
+ship --vm compress debian-vm-base 
+
+echo "Starting the xz compression of the Debian disk image to generate the release package for 'debian-vm-base'..."
+
+DISK_IMAGE=$(sudo virsh domblklist debian-vm-base | grep .qcow2 | awk '{print $2}')
+
+xz -9 -z "$DISK_IMAGE"
+
+echo "Moving the compressed disk image to the output directory..."
+
+mv "$DISK_IMAGE.xz" output/debian.qcow2.xz
+
+echo "The release package for 'debian-vm-base' has been generated successfully!"
